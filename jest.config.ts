2f5e4b862f9afbf1b5c9569845aae40e21fe2600
Luis <46550513+Lichos17@users.noreply.github.com>
2023-01-24 22:38:53 -0600

    refactor: add functions for each property VehicleModel

--- jest.config.ts
+++ jest.config.ts
@@ -1,12 +1,12 @@
+module.exports = {
 
-export default {
-  roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/***/*.ts'],
-  collectCoverage: true,
+  roots: ['<rootDir>/src/_tests_'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.{js,jsx,ts,tsx}'],
   coverageDirectory: 'coverage',
-  coverageProvider: 'v8',
+  // coverageProvider: "v8",
   testEnvironment: 'node',
   transform: {
-    '.*\\.ts$': 'ts-jest'
+    '.+\\.ts$': 'ts-jest'
   }
+
 }
