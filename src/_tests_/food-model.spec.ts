b22ac6e9de491621f488fb01da5f883a5d00abd4
Luis <46550513+Lichos17@users.noreply.github.com>
2023-01-24 23:16:29 -0600

    refactor: add FoodHelper inject constructor FoodModel

--- src/_tests_/food-model.spec.ts
+++ src/_tests_/food-model.spec.ts
@@ -2,10 +2,11 @@ import { Food } from '../models/food-model'
 
 describe('Classes', () => {
   it('Class Food', () => {
-    const food = new Food('name', 'description', 50)
+    const food = new Food({ name: 'name', description: 'description', price: 50, inventory: 10 })
 
     expect(food.getName()).toEqual('name')
     expect(food.getDescription()).toEqual('description')
     expect(food.getPrice()).toEqual(50)
+    expect(food.getInventory()).toEqual(10)
   })
 })
--- src/models/food-model.ts
+++ src/models/food-model.ts
@@ -1,13 +1,16 @@
+import { FoodHelper } from '../helpers/food-model-helper'
+
 export class Food {
-  constructor (private readonly name: string, private readonly description: string, private readonly price: number) {}
+  constructor (private readonly food: FoodHelper) {}
 
   getName (): string {
-    return this.name
+    return this.food.name
   }
 
   getDescription (): string {
-    return this.description
+    return this.food.description
   }
 
-  getPrice (): number { return this.price }
+  getPrice (): number { return this.food.price }
+  getInventory (): number { return this.food.inventory }
 }
