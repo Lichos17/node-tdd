7436e3023aeb24f2c73c7131c8953fb129089aa3
Luis <46550513+Lichos17@users.noreply.github.com>
2023-01-24 22:54:19 -0600

    test: add test class FoodModel

---
+++ src/_tests_/food-model.spec.ts
@@ -0,0 +1,11 @@
+import { Food } from '../models/food-model'
+
+describe('Classes', () => {
+  it('Class Food', () => {
+    const food = new Food('name', 'description', 50)
+
+    expect(food.getName()).toEqual('name')
+    expect(food.getDescription()).toEqual('description')
+    expect(food.getPrice()).toEqual(50)
+  })
+})
